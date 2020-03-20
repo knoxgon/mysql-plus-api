c319c62c0fa29193541ba45205de35ba32af69bc
knoxgon <volkan.guven@hotmail.se>
2020-03-20 10:42:20 +0100

    Corrected input resolution for todolist

--- db/procedures.js
+++ db/procedures.js
@@ -3,7 +3,7 @@ const connection = require('./index');
 //Select everything from table and send
 module.exports.runTranscationSelectAll = (table) => {
   return new Promise((resolve, reject) => {
-    connection.query('SELECT * FROM $1;', [table], (err,rows) => {
+    connection.query('SELECT * FROM ??', [table], (err,rows) => {
       if(err) reject(`Error occured while processing data from ${table};`);
       resolve(rows)
     });
